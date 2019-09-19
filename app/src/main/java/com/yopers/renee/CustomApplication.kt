56e2530b80aa76740c21fc373d3b1fe580a9971a
ParitoshBh <mail@paritoshbh.me>
2019-09-19 02:28:53 +0000

    Include Timber logging library

--- app/src/main/java/com/yopers/renee/CustomApplication.kt
+++ app/src/main/java/com/yopers/renee/CustomApplication.kt
@@ -3,6 +3,7 @@ package com.yopers.renee
 import android.app.Application
 import com.bugsnag.android.Bugsnag
 import io.paperdb.Paper
+import timber.log.Timber
 
 class CustomApplication: Application() {
     override fun onCreate() {
@@ -10,5 +11,8 @@ class CustomApplication: Application() {
 
         Paper.init(this)
         Bugsnag.init(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
